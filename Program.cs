b70b92aab43e3798da7ed88379117344cebd735f
Patrick Herman Andrade da Conceicao Ens <patrickhacens@gmail.com>
2017-10-05 13:05:17 -0300

    init

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ConsoleCadastro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int idade;
+
+            Console.WriteLine("Qual é a sua idade?");
+
+            string usuarioEscreveu = Console.ReadLine();
+
+
+            idade = int.Parse(usuarioEscreveu);
+
+            Console.Write("Sua idade é:"); 
+
+            Console.Write(idade);
+        }
+    }
+}
